26f3c1316e09ad53e731b3e6a2e01bc8fc161baa
Anothny Karahalios <akarahal@fac-vanhoeve02.tepper.cmu.edu>
2023-09-07 10:54:23 -0400

    add run instance scripts

---
+++ scripts/run_instance1.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+export LD_LIBRARY_PATH="$HOME/dd_vrptw/Cliquer/src/lib/:$LD_LIBRARY_PATH"
+
+if [ $# -ne 9 ]; then
+  echo "Invalid arguments"
+  echo "Argument 1: instances dir"
+  echo "Argument 2: COL_GEN or COL_ELIM"
+  echo "Argument 3: DD/DP/LP/LAG"
+  echo "Argument 4: Q/NG"
+  echo "Argument 5: s/k value"
+  echo "Argument 6: max s value"
+  echo "Argument 7: use cuts"
+  echo "Argument 8: timeout"
+  echo "Argument 9: description"
+  exit 2
+fi
+
+OUTPUT_DIR=$HOME/dd_vrptw/logs/$9/
+META_FILE=$OUTPUT_DIR/meta.log
+mkdir -p $OUTPUT_DIR
+echo "Arguments $1, $2, $3, $4, $5, $6, $7, $8, $9" > $META_FILE
+
+FILES=$HOME/dd_vrptw/$1/*
+for f in $FILES
+do
+  echo "Processing $f file..."
+
+  OUTPUT_FILE=$OUTPUT_DIR/$(basename $f).log
+  if [ -f "$OUTPUT_FILE" ]; then
+    echo "$OUTPUT_FILE does exist."
+  else
+    echo "Running for $OUTPUT_FILE"
+    timeout $8 .././solver_novarfix $f $2 $3 $4 $5 $6 $7 $8 > $OUTPUT_FILE
+  fi
+done
---
+++ scripts/run_instance_86400.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+export LD_LIBRARY_PATH="$HOME/dd_vrptw/Cliquer/src/lib/:$LD_LIBRARY_PATH"
+
+if [ $# -ne 3 ]; then
+  echo "Invalid arguments"
+  echo "Argument 1: instances dir"
+  echo "Argument 2: COL_GEN or COL_ELIM"
+  echo "Argument 3: param file name"
+  exit 2
+fi
+
+OUTPUT_DIR=$HOME/dd_vrptw/logs/$1_$3/
+mkdir -p $OUTPUT_DIR
+
+FILES=$HOME/dd_vrptw/instances/$1/*
+PARAM_FILE=$HOME/dd_vrptw/param_files/$3.csv
+for f in $FILES
+do
+  echo "Processing $f file..."
+
+  OUTPUT_FILE=$OUTPUT_DIR/$(basename $f).log
+  if [ -f "$OUTPUT_FILE" ]; then
+    echo "$OUTPUT_FILE does exist."
+  else
+    echo "Running for $OUTPUT_FILE"
+    timeout 86400 .././solver $f $2 $PARAM_FILE > $OUTPUT_FILE
+  fi
+done
